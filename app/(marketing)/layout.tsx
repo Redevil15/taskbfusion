e8af1e1f839c895f51f52d532e40cbce71d8cd6d
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-12-20 13:00:39 -0600

    Feat: add footer to main page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Navbar } from "./_components/navbar";
 
 const MarketingLayout = ({
@@ -8,11 +9,11 @@ const MarketingLayout = ({
   return (
     <div className="h-full bg-slate-100">
 
-      <Navbar/>
+      <Navbar />
       <main className="pt-40 pb-20 bg-slate-100">
         {children}
       </main>
-      {/* Footer */}
+      <Footer />
     </div>
   )
 }

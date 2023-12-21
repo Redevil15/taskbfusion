411783a2c8bb181e60f880e726c24826837d229a
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-12-21 11:44:59 -0600

    Feat: add some shadcn components, edit nextConfig for clerkImages and add navItem component

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,3 +1,5 @@
+import { Sidebar } from "../_components/sidebar";
+
 const OrganizationLayout = ({
   children
 }: {
@@ -7,7 +9,7 @@ const OrganizationLayout = ({
     <div className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
       <div className="flex gap-x-7">
         <div className="w-64 shrink-0 hidden md:block">
-          {/* Sidebar */}
+          <Sidebar />
         </div>
         {children}
       </div>

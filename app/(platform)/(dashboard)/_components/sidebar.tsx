2604d512b9eaaf8f2a22487bb2dbcd34ec3a728a
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2023-12-21 18:03:45 -0600

    Feat: add skeleton for the organization accordion

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -61,6 +61,11 @@ export const Sidebar = ({
           <Skeleton className="h-10 w-[50%]" />
           <Skeleton className="h-10 w-10" />
         </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     )
   }

6fdb6a99a3e0ea0eca4f9e5ba394b37438f40f9a
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-17 15:19:34 -0600

    Fix: error indicator in board title input

--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -30,9 +30,7 @@ export const UseAction = <TInput, TOutput>(
           return;
         }
 
-        if (result.fieldErrors) {
-          setFieldErrors(result.fieldErrors)
-        }
+        setFieldErrors(result.fieldErrors)
 
         if (result.error) {
           setError(result.error);

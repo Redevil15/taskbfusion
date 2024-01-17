12b20cf99b12ced01db2beb58a3fd3855e871c7b
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-17 16:41:35 -0600

    Feat: add create board popover

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,3 +1,4 @@
+import { FormPopover } from "@/components/form/form-popover"
 import { Hint } from "@/components/hint"
 import { HelpCircleIcon, User2Icon } from "lucide-react"
 
@@ -10,25 +11,28 @@ export const BoardList = () => {
         Your boards
       </div>
       <div className="grid gird-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div
-          role="button"
-          className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
-        >
-          <p className="text-sm">Create new board</p>
-          <span className="">
-            5 remaining
-          </span>
-          <Hint
-            sideOffset={40}
-            description={`
+        <FormPopover sideOffset={10} side="right">
+          <div
+            role="button"
+            className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+          >
+            <p className="text-sm">Create new board</p>
+            <span className="">
+              5 remaining
+            </span>
+            <Hint
+              sideOffset={40}
+              description={`
               Free workspaces can have up to 2 open boards. For unlimited boards upgrade this workspace
             `}
-          >
-            <HelpCircleIcon
-              className="absolute bottom-2 right-2 h-[14px] w-[14px]"
-            />
-          </Hint>
-        </div>
+            >
+              <HelpCircleIcon
+                className="absolute bottom-2 right-2 h-[14px] w-[14px]"
+              />
+            </Hint>
+          </div>
+        </FormPopover>
+
       </div>
     </div>
   )

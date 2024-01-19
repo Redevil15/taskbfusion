b8c5c77dca4da3320cc9d326ff458011eb8ed519
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-19 14:50:48 -0600

    Feat: add list actions options in moreHorizontal popover menu

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -9,7 +9,9 @@ import {
   PopoverClose
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
-import { MoreHorizontal } from "lucide-react";
+import { MoreHorizontal, X } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { FormSubmit } from "@/components/form/form-submit";
 
 interface ListOptionsProps {
   data: List,
@@ -27,6 +29,45 @@ export const ListOptions = ({
           <MoreHorizontal className="h-4 w-4" />
         </Button>
       </PopoverTrigger>
+      <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          List actions
+        </div>
+        <PopoverClose asChild>
+          <Button className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600" variant="ghost">
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <Button
+          onClick={onAddCard}
+          className="w-full rounded-none h-auto p-2 px-5 justify-start font-normal text-sm"
+          variant="ghost"
+        >
+          Add card...
+        </Button>
+
+        <form action="">
+          <input hidden name="id" id="id" value={data.id} />
+          <input hidden name="boardId" id="boardId" value={data.boardId} />
+          <FormSubmit
+            variant="ghost"
+            className="w-full rounded-none h-auto p-2 px-5 justify-start font-normal text-sm"
+          >
+            Copy list...
+          </FormSubmit>
+        </form>
+        <Separator />
+        <form action="">
+          <input hidden name="id" id="id" value={data.id} />
+          <input hidden name="boardId" id="boardId" value={data.boardId} />
+          <FormSubmit
+            variant="ghost"
+            className="w-full rounded-none h-auto p-2 px-5 justify-start font-normal text-sm"
+          >
+            Delete this list...
+          </FormSubmit>
+        </form>
+      </PopoverContent>
     </Popover>
   )
 }

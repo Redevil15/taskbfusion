95aed59d79fd77816daac2d52b8f65a687b91987
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-19 10:52:35 -0600

    Feat: add create-list action

---
+++ actions/create-list/schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod"
+
+export const CreateList = z.object({
+  title: z.string({
+    required_error: "Title is required",
+    invalid_type_error: "Title is required"
+  }).min(2, {
+    message: "Title must be at least 2 characters long"
+  }),
+  boardId: z.string(),
+});

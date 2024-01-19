c9a4a11455014669ef423c96c604d34d48ade163
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-19 17:58:19 -0600

    Feat: add create card action

---
+++ actions/create-card/schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod"
+
+export const CreateCard = z.object({
+  title: z.string({
+    required_error: "Title is required",
+    invalid_type_error: "Title is required"
+  }).min(3, {
+    message: "Title must be at least 2 characters long"
+  }),
+  boardId: z.string(),
+  listId: z.string(),
+});

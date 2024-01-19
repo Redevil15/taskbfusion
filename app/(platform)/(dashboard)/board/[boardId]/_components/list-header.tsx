95b9925f5240254d86f3c493925dab3b9f1bd8d1
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-19 12:16:37 -0600

    Feat: create update list title and functionality to my form inside list-header

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -3,6 +3,9 @@ import { useState, useRef, ElementRef } from "react"
 import { List } from "@prisma/client"
 import { useEventListener } from "usehooks-ts"
 import { FormInput } from "@/components/form/form-input"
+import { UseAction } from "@/hooks/use-action"
+import { updateList } from "@/actions/uptade-list"
+import { toast } from "sonner"
 
 interface ListHeaderProps {
   data: List
@@ -29,6 +32,37 @@ export const ListHeader = ({
     setIsEditing(false)
   };
 
+  const { execute } = UseAction(updateList, {
+    onSuccess: (data) => {
+      toast.success(`Renamed to "${data.title}"`);
+      setTitle(data.title)
+      disableEditing()
+    },
+    onError: (error) => {
+      toast.error(error)
+    }
+  });
+
+  const handleSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string
+    const id = formData.get("id") as string
+    const boardId = formData.get("boardId") as string
+
+    if (title === data.title) {
+      return disableEditing();
+    }
+
+    execute({
+      title,
+      id,
+      boardId,
+    })
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === "Escape") {
       formRef.current?.requestSubmit();
@@ -40,7 +74,11 @@ export const ListHeader = ({
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
       {isEditing ? (
-        <form className="flex-1 px-[2px]">
+        <form
+          ref={formRef}
+          action={handleSubmit}
+          className="flex-1 px-[2px]"
+        >
           <input
             hidden
             id="id"
@@ -53,12 +91,13 @@ export const ListHeader = ({
           />
           <FormInput
             ref={inputRef}
-            onBlur={() => { }}
+            onBlur={onBlur}
             id="title"
             placeholder="Enter list title..."
             defaultValue={title}
             className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition truncate bg-transparent focus:bg-white"
           />
+          <button type="submit" hidden />
         </form>
       ) : (
         <div

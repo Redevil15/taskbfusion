e84c5549c6f93152d602a5bbdd08ccf21af90fb4
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-19 18:48:31 -0600

    Feat: give a button role to my card item div

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -11,6 +11,7 @@ export const CardItem = ({
 }: CardItemProps) => {
   return (
     <div
+      role="button"
       className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
     >
       {data.title}

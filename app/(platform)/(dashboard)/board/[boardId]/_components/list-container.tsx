7b6913e35f68efbdbd2e8831736bbae15ff56603
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-19 19:09:47 -0600

    Feat: add gragable ui - just for visual(no functionality locally or back)

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,9 +1,11 @@
 "use client"
 
-import { ListWithCards } from "@/types";
-import { List } from "@prisma/client"
-import { ListForm } from "./list-form";
 import { useEffect, useState } from "react";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+
+import { ListWithCards } from "@/types";
+
+import { ListForm } from "./list-form";
 import { ListItem } from "./list-item";
 
 interface ListContainerProps {
@@ -24,18 +26,36 @@ export const ListContainer = ({
 
 
   return (
-    <ol className="flex gap-x-3 h-fulll">
-      {orderedData.map((list, index) => {
-        return (
-          <ListItem
-            key={list.id}
-            index={index}
-            data={list}
-          />
-        )
-      })}
-      <ListForm />
-      <div className="flex-shrink-0 w-1" />
-    </ol>
+    <DragDropContext
+      onDragEnd={() => { }}
+    >
+      <Droppable
+        droppableId="lists"
+        type="list"
+        direction="horizontal"
+      >
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-fulll"
+          >
+            {orderedData.map((list, index) => {
+              return (
+                <ListItem
+                  key={list.id}
+                  index={index}
+                  data={list}
+                />
+              )
+            })}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+
+    </DragDropContext>
   )
 }

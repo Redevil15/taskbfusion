2e9ba0d3dba81deccb03cf3eaf0edfc68d6de632
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-22 17:05:15 -0600

    Feat: add actions buttons (just ui)

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -8,6 +8,7 @@ import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
 import { Header } from "./header";
 import { Description } from "./description";
+import { Actions } from "./actions";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -40,6 +41,11 @@ export const CardModal = () => {
               }
             </div>
           </div>
+          {
+            !cardData
+              ? <Actions.Skeleton />
+              : <Actions data={cardData} />
+          }
         </div>
 
       </DialogContent>

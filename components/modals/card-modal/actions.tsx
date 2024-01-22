87b4686b1dffe6bf95b22b5b996ab69e01026713
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-22 17:28:07 -0600

    Feat: add copy and delete cards functionality

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -5,6 +5,14 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { CardWithList } from "@/types";
 import { Copy, Trash } from "lucide-react";
 
+import { UseAction } from "@/hooks/use-action";
+import { copyCard } from "@/actions/copy-card";
+import { deleteCard } from "@/actions/delete-card";
+import { useParams } from "next/navigation";
+import { useCardModal } from "@/hooks/use-card-modal";
+import { toast } from "sonner";
+import { error } from "console";
+
 interface ActionsProps {
   data: CardWithList
 }
@@ -12,12 +20,61 @@ interface ActionsProps {
 export const Actions = ({
   data
 }: ActionsProps) => {
+  const {
+    execute: executeCopyCard,
+    isLoading: isLoadingCopy
+  } = UseAction(copyCard, {
+    onSuccess: () => {
+      toast.success(`Card "${data.title}" copied.`)
+      cardModal.onClose();
+    },
+    onError: (error) => {
+      toast.error(error)
+    }
+  });
+
+  const {
+    execute: executeDeleteCard,
+    isLoading: isLoadingDelete
+  } = UseAction(deleteCard, {
+    onSuccess: () => {
+      toast.success(`Card "${data.title}" deleted.`)
+      cardModal.onClose();
+    },
+    onError: (error) => {
+      toast.error(error)
+    }
+  });
+
+  const params = useParams();
+  const cardModal = useCardModal();
+
+  const onCopy = () => {
+    const boardId = params.boardId as string;
+
+    executeCopyCard({
+      id: data.id,
+      boardId,
+    });
+  }
+
+  const onDelete = () => {
+    const boardId = params.boardId as string;
+
+    executeDeleteCard({
+      id: data.id,
+      boardId,
+    });
+  }
+
   return (
     <div className="space-y-2 mt-2">
       <p className="text-xs font-semibold">
         Actions
       </p>
       <Button
+        onClick={onCopy}
+        disabled={isLoadingCopy}
         variant="gray"
         className="w-full justify-start"
         size="inline"
@@ -26,6 +83,8 @@ export const Actions = ({
         Copy
       </Button>
       <Button
+        onClick={onDelete}
+        disabled={isLoadingDelete}
         variant="gray"
         className="w-full justify-start"
         size="inline"

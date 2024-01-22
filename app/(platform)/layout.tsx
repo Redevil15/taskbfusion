517a4dd829cce2def4b28f2ae2391d27435b5f79
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-22 10:31:23 -0600

    Feat: create a queryProvider and wrap the entire platform

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,4 +1,5 @@
 import { ModalProvider } from "@/components/providers/modal-provider"
+import { QueryProvider } from "@/components/providers/query-provider"
 import { ClerkProvider } from "@clerk/nextjs"
 import { Toaster } from "sonner"
 
@@ -9,9 +10,11 @@ const PlatformLayout = ({
 }) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      <ModalProvider />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   )
 }

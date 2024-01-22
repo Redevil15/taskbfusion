b24d456870f827cf62ee41da06cee874e63b43ca
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-22 10:43:37 -0600

    Feat: create a fetcher && api route to receive my cardData

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useCardModal } from "@/hooks/use-card-modal";
+import { CardWithList } from "@/types";
+import { fetcher } from "@/lib/fetcher";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
   const isOpen = useCardModal((state) => state.isOpen);
   const onClose = useCardModal((state) => state.onClose);
+
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
+
 
   return (
     <Dialog
@@ -14,7 +24,7 @@ export const CardModal = () => {
       onOpenChange={onClose}
     >
       <DialogContent>
-        I am a modal
+        {cardData?.title}
       </DialogContent>
     </Dialog>
   );

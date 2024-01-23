2eff1d7b82123234a71389bc8781bb0e8f7663dd
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-01-23 09:26:52 -0600

    Fix: cardModal responsive

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -2,14 +2,15 @@
 
 import { useQuery } from "@tanstack/react-query";
 
-import { Dialog, DialogContent } from "@/components/ui/dialog";
-import { useCardModal } from "@/hooks/use-card-modal";
 import { CardWithList } from "@/types";
 import { fetcher } from "@/lib/fetcher";
+import { AuditLog } from "@prisma/client";
+import { useCardModal } from "@/hooks/use-card-modal";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+
 import { Header } from "./header";
 import { Description } from "./description";
 import { Actions } from "./actions";
-import { AuditLog } from "@prisma/client";
 import { Activity } from "./activity";
 
 export const CardModal = () => {
@@ -27,7 +28,6 @@ export const CardModal = () => {
     queryFn: () => fetcher(`/api/cards/${id}/logs`),
   });
 
-
   return (
     <Dialog
       open={isOpen}
@@ -41,25 +41,21 @@ export const CardModal = () => {
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
           <div className="col-span-3">
             <div className="w-full space-y-6">
-              {
-                !cardData
-                  ? <Description.Skeleton />
-                  : <Description data={cardData} />
+              {!cardData
+                ? <Description.Skeleton />
+                : <Description data={cardData} />
+              }
+              {!auditLogsData
+                ? <Activity.Skeleton />
+                : <Activity items={auditLogsData} />
               }
             </div>
           </div>
-          {
-            !cardData
-              ? <Actions.Skeleton />
-              : <Actions data={cardData} />
-          }
-          {
-            !auditLogsData
-              ? <Activity.Skeleton />
-              : <Activity items={auditLogsData} />
+          {!cardData
+            ? <Actions.Skeleton />
+            : <Actions data={cardData} />
           }
         </div>
-
       </DialogContent>
     </Dialog>
   );
